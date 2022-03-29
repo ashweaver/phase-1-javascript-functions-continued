9900639074beeaff6e54ed4f1d9b9ae71ef9cfa7
ashweaver <97565194+ashweaver@users.noreply.github.com>
2022-03-29 08:08:40 -0500

    commit tree dca9c2a4f68f945d4f3f4cbc66619f17b0eb58f8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "bathe my dog!"){
+    return 'This Saturday I want to ${activity}!'
+}
+
+let mondayWork = function (acvtivity="I will work from home"){
+    return 'This Monday, I will ${activity}.'
+
+}
+let wrapAdjective = function (whatever= "*"){
+    return function (a = "special"){
+        return 'You are ${a}${whatever}${a}!'
+    }
+    const encouragingPromptFunction = wrapAdjective ("!!!")
+}
+wrapAdjective("*")("a dedicated programmer");
